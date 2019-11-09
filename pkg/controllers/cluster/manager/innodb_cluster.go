d7c0258d264d8848e76427ef76fe4236dee6a3a7
houruiming <houruiming1@jd.com>
2019-11-09 08:42:35 +0800

    fix: create or rejoin cluster much faster in kubernetes pod on different hosts

--- pkg/controllers/cluster/manager/innodb_cluster.go
+++ pkg/controllers/cluster/manager/innodb_cluster.go
@@ -35,7 +35,7 @@ import (
 
 var errNoClusterFound = errors.New("no cluster found on any of the seed nodes")
 
-const defaultTimeout = 10 * time.Second
+const defaultTimeout = 120 * time.Second //todo increased to avoid signal kill error
 
 // isDatabaseRunning returns true if a connection can be made to the MySQL
 // database running in the pod instance in which this function is called.
